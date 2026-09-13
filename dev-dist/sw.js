a185695c61ca3cf42f1e37ab9c8572a492535082
Lam <tranthithulam0510@gmail.com>
2026-09-14 00:25:52 +0700

    Fix logo size

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b9db2e3b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lo0flhk1as"
+    "revision": "0.tkj3r9e4ub4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
